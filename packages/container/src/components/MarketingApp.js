ed84596ad39a3de1afbae1b5b2826e1311db5ec6
Wei Jie Phua <weijie_luvbb@hotmail.com>
2022-08-08 23:35:30 +0800

    trigger pipeline

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -2,7 +2,7 @@ import { mount } from 'marketing/MarketingApp'
 import React, { useRef, useEffect } from 'react'
 
 export default () => {
-  const ref = useRef(null)
+  const ref = useRef(null) 
 
   useEffect(() => {
     mount(ref.current)
