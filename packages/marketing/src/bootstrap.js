eb2c3ce712af270d9997ec1a2f6cbc082dff7fb2
Wei Jie Phua <weijie_luvbb@hotmail.com>
2022-08-08 23:36:18 +0800

    trigger pipeline

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -9,7 +9,7 @@ const mount = (el) => {
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root')
   if (devRoot) {
-    mount(devRoot)
+    mount(devRoot) 
   }
 }
 
