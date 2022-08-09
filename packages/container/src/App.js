be5a6a031fdf473b81c31233efbbead3615a42f3
Wei Jie Phua <weijie_luvbb@hotmail.com>
2022-08-09 14:29:30 +0800

    add production prefix random classanme to prevent same classname in production environment

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+})
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
